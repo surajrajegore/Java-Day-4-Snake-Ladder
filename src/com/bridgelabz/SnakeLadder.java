00a8ac9c42b8a97e57595726a7736a8cd401b940
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-14 16:25:47 +0530

    Added:current position 0

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,6 +3,10 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args)
     {
+        int pos=0;
+
         System.out.println("Welcome to Snake and Ladder..");
+        
+
     }
 }
