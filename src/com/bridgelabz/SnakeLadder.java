bd61424ddd633d241e17c5a125d2d6668f0e4330
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-14 16:37:33 +0530

    Added:code of dice 1-6

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -6,7 +6,8 @@ public class SnakeLadder {
         int pos=0;
 
         System.out.println("Welcome to Snake and Ladder..");
-        
+
+            int dice = (int)(1+Math.random()*6);
 
     }
 }
