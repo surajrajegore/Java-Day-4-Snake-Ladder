82dbc7c2fe7ecab9640f45bf61cb8a50efd9d147
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-24 23:25:45 +0530

    Added:code repeat until 100

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,25 +4,31 @@ public class SnakeLadder {
     public static void main(String[] args)
     {
         int position = 0;
-        int playerPlay = (int) (Math.random() * 2);
-        final int SNAKE = 1;
-        final int NOPLAY = 2;
-        final int LADDER = 3;
-        int options = (int) (Math.random() * 4);
-        switch (options) {
-            case LADDER:
-                position += playerPlay;
-                System.out.println("the player got Ladder:" + position);
-                break;
-            case NOPLAY:
-                System.out.println("the player remains in same position:" + position);
-                break;
-            case SNAKE:
-                position -= playerPlay;
-                System.out.println("the snake bit the player:" + position);
-                break;
-        }
-        System.out.println("position is :" + position);
+        do {
+            int playerPlay = (int) (Math.random() * 2);
+            final int SNAKE = 1;
+            final int NOPLAY = 2;
+            final int LADDER = 3;
+            int options = (int) (Math.random() * 4);
+            switch (options) {
+                case LADDER:
+                    position += playerPlay;
+                    System.out.println("the player got Ladder:" + position);
+                    break;
+                case NOPLAY:
+                    System.out.println("the player remains in same position:" + position);
+                    break;
+                case SNAKE:
+                    position -= playerPlay;
+                    System.out.println("the snake bit the player:" + position);
+                    break;
+            }
+            System.out.println("position is :" + position);
+            if (position<0)
+            {
+                position=0;
+            }
+        }while (position<100);
         }
     }
 
