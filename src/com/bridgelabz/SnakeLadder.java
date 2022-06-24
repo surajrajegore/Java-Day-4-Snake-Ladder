37a6bf2aeda5d2e8d44870f0e05dc734cda8f433
surajrajegore <rajegoresuraj2880@gmail.com>
2022-06-24 23:07:47 +0530

    Added:code to Play or not play

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,11 +3,26 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args)
     {
-        int pos=0;
-
-        System.out.println("Welcome to Snake and Ladder..");
-
-            int dice = (int)(1+Math.random()*6);
-
+        int position = 0;
+        int playerPlay = (int) (Math.random() * 2);
+        final int SNAKE = 1;
+        final int NOPLAY = 2;
+        final int LADDER = 3;
+        int options = (int) (Math.random() * 4);
+        switch (options) {
+            case LADDER:
+                position += playerPlay;
+                System.out.println("the player got Ladder:" + position);
+                break;
+            case NOPLAY:
+                System.out.println("the player remains in same position:" + position);
+                break;
+            case SNAKE:
+                position -= playerPlay;
+                System.out.println("the snake bit the player:" + position);
+                break;
+        }
+        System.out.println("position is :" + position);
+        }
     }
-}
+
